f028f6aabbcedaec21489ece8df83ad982e8c858
KrononConspirator <apexconspirator_367@outlook.com>
2020-10-04 21:24:54 +0800

    added item system

---
+++ BlackSmith/Assets/Code/ScriptableObject/ItemManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ItemType
+{
+    _WEAPON,
+    _POTION,
+    _INGREDIENT,
+    _ARMOR,
+    _EQUIPMENT,
+    _MISC
+}
+
+public abstract class ItemManager : ScriptableObject
+{
+    public new string name;
+    public ItemType Type;
+    [TextArea(15,20)]
+    public string description;
+    public GameObject prefab;
+}
---
+++ BlackSmith/Assets/Code/ScriptableObject/ItemWeaponManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum Weapon_T
+{
+	UNARMED = 0,
+	TWOHANDSWORD = 1,
+	TWOHANDSPEAR = 2,
+	TWOHANDAXE = 3,
+	TWOHANDBOW = 4,
+	TWOHANDCROSSBOW = 5,
+	STAFF = 6,
+	ARMED = 7,
+	RELAX = 8,
+	RIFLE = 9,
+	TWOHANDCLUB = 10,
+	SHIELD = 11,
+	ARMEDSHIELD = 12
+}
+public class ItemWeaponManager : ItemManager
+{
+    public int damage;
+    public Weapon_T Weapon_T;
+
+    public Vector3 HandleRotation;
+    public Vector3 HandleLocation;
+
+
+}
